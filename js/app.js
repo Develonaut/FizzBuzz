38d7ecf1dfbd5e9338e94eec3eb94f37a3b33465
Ryan McHenry <ryanmmchenry@gmail.com>
2014-08-13 22:17:57 -0400

    Fixed: Check it out button, and exit list button

--- js/app.js
+++ js/app.js
@@ -1,33 +1,17 @@
-	
-$("#entryBox").hide();
-$("#exitBox").hide();
 
-$("#addNumber").on("click", function(){
-	$(".introBox").fadeOut();
-	$("#entryBox").fadeIn("fast",function() {
-		$("#exitBox").fadeIn();
-	});
-});
-
-$("#exitBox").on("click", function(){
-	$(".introBox").fadeIn();
-	$("#entryBox").fadeOut("fast",function() {
-		$("#exitBox").fadeOut();
-	});
-});
-
-
-
-
+// Variable stored to remove magic numbers
+var fizz = 3;
+var buzz = 5;
+var pause = 1200;
 
 // Loop that runs the the number given
 function test(choice) {
 	for (var i = 1; i <= choice; i++) {
-		if (i % 3 === 0 && i % 5 === 0)  {
+		if (i % fizz === 0 && i % buzz === 0)  {
 			$("#entryList").append("<li>fizzbuzz</li>");
-		} else if (i % 3 === 0) {
+		} else if (i % fizz === 0) {
 			$("#entryList").append("<li>fizz</li>");
-		} else if (i % 5 === 0){
+		} else if (i % buzz === 0){
 			$("#entryList").append("<li>buzz</li>");
 		} else {
 			$("#entryList").append("<li>" + i + "</li>");
@@ -35,5 +19,67 @@ function test(choice) {
 	}
 }
 
-test();
-	
+var $inputValue = $("#userChoice");
+
+// function isNumber() {
+// 	if ($inputValue.val() === )
+// }
+
+$(".numberMessage").hide();
+$("#entryBox").hide();
+$("#exitBox").hide();
+
+$("#addNumber").on("click", function(){
+	if ($inputValue.val().trim() !== "" && !isNaN($inputValue.val().trim())) {
+		$(".introBox").fadeOut();
+		test($inputValue.val());
+		$("#entryBox").fadeIn("fast",function() {
+			$("#exitBox").fadeIn(function () {
+			});
+		});
+	} else if (isNaN($inputValue.val().trim())) {
+		$(".numberMessage").fadeIn("fast").delay(1200).fadeOut();
+	}
+});
+
+$("#exitBox").on("click", function(){
+	$(".introBox").fadeIn();
+	$("#entryBox").fadeOut("fast",function() {
+		$("#exitBox").fadeOut(function () {
+			$("li").remove();
+		});
+	});
+});
+
+
+$(document).keyup(function(event) {
+	if(event.keyCode === 13) {
+		if ($inputValue.val().trim() !== "" && !isNaN($inputValue.val().trim())) {
+			$(".introBox").fadeOut();
+			test($inputValue.val());
+			$("#entryBox").fadeIn("fast",function() {
+			$("#exitBox").fadeIn(function () {
+		});
+	});
+		} else if (isNaN($inputValue.val().trim())) {
+		$(".numberMessage").fadeIn("fast").delay(1200).fadeOut();
+		}
+	}
+});
+
+
+    var counter = $('.counter');
+    
+    $('#userChoice').on('input keyup keydown focus', function () {
+            var $this = $(this);
+            var maxlength = $this.attr('maxlength');
+            var value = $this.val();
+    
+            if(value.length > 0) {
+                counter.text((maxlength - $this.val().length));
+            } else {
+                counter.text(maxlength);
+            }   
+        });
+
+
